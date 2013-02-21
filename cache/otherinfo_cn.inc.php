5ed367689eb5cf21100eb3413400b6e9c89d985c
justinhuang917 <justinhuang917@gmail.com>
2013-02-21 10:35:30 +0800

    initialize

---
+++ cache/otherinfo_cn.inc.php
@@ -0,0 +1,33 @@
+<?php
+ return array (
+  'id' => '3',
+  'info1' => '',
+  'info2' => '',
+  'info3' => '',
+  'info4' => '',
+  'info5' => '',
+  'info6' => '',
+  'info7' => '',
+  'info8' => '',
+  'info9' => '',
+  'info10' => '<div>
+	<span style="line-height: 2;">中国国学产品有限公司</span></div>
+<div>
+	电 &nbsp;话：0755-8888888</div>
+<div>
+	邮 &nbsp;编：518000</div>
+<div>
+	Email：admin@gx.com</div>
+<div>
+	网 &nbsp;址：www.gx.com</div>
+',
+  'imgurl1' => '',
+  'imgurl2' => '',
+  'rightmd5' => '',
+  'righttext' => '',
+  'authcode' => '',
+  'authpass' => '',
+  'authtext' => '',
+  'data' => '',
+  'lang' => 'cn',
+); ?>
